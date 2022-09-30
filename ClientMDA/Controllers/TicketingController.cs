88aa986aee70f107c913d1f74589828bd39260b9
annlan08 <annlan08@users.noreply.github.com>
2022-09-30 15:51:31 +0800

    加入專案檔案。

---
+++ ClientMDA/Controllers/TicketingController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ClientMDA.Controllers
+{
+    public class TicketingController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult MovieInfoIndex()
+        {
+            return View();
+        }
+        public IActionResult MovieInfoIndex2()
+        {
+            return View();
+        }
+
+        public IActionResult SeatMap()
+        {
+            return View();
+        }
+
+        public IActionResult PaymentWeb()
+        {
+            return View();
+        }
+
+        public IActionResult PaymentWeb2()
+        {
+            return View();
+        }
+    }
+}
